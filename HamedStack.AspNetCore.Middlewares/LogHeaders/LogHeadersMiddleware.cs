7d1f22344e6159e1ad42ea1deb4ca62205db5e80
hamedfathi <hamed.fathi@ringana.com>
2024-02-01 13:15:49 +0100

    feat(Middlewares): LogHeadersMiddleware added.

---
+++ HamedStack.AspNetCore.Middlewares/LogHeaders/LogHeadersMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HamedStack.AspNetCore.Middlewares.LogHeaders;
+
+/// <summary>
+/// Middleware component for logging HTTP request headers.
+/// This class provides functionality to log each header of incoming HTTP requests.
+/// </summary>
+public class LogHeadersMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<LogHeadersMiddleware> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LogHeadersMiddleware"/> class.
+    /// </summary>
+    /// <param name="next">The next middleware component in the pipeline.</param>
+    /// <param name="logger">The logger used for logging information.</param>
+    public LogHeadersMiddleware(RequestDelegate next, ILogger<LogHeadersMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Processes an individual request.
+    /// </summary>
+    /// <param name="context">The context for the current HTTP request.</param>
+    /// <returns>A Task representing the asynchronous operation.</returns>
+    public async Task InvokeAsync(HttpContext context)
+    {
+        foreach (var header in context.Request.Headers)
+        {
+            _logger.LogInformation("Header: {Key}: {Value}", header.Key, header.Value);
+        }
+
+        await _next(context);
+    }
+}
---
+++ HamedStack.AspNetCore.Middlewares/LogHeaders/LogHeadersMiddlewareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace HamedStack.AspNetCore.Middlewares.LogHeaders;
+
+/// <summary>
+/// Extension methods for adding the LogHeadersMiddleware to the application's request pipeline.
+/// </summary>
+public static class LogHeadersMiddlewareExtensions
+{
+    /// <summary>
+    /// Adds the LogHeadersMiddleware to the application's request pipeline.
+    /// </summary>
+    /// <param name="builder">The IApplicationBuilder instance.</param>
+    /// <returns>The IApplicationBuilder instance with the middleware configured.</returns>
+    public static IApplicationBuilder UseLogHeaders(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<LogHeadersMiddleware>();
+    }
+}
